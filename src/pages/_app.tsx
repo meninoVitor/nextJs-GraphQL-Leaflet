4376a720653fb57f3b884899c617b1051a5acaff
Vitor Lemos <lemos.dev02@gmail.com>
2021-09-19 00:59:34 -0300

    Add head meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
-import type { AppProps } from 'next/app'
 import React from 'react'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import GlobalStyle from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>React avançado - Boilerplate</title>
+        <meta
+          name="description"
+          content="A simple react app to starter work with React, TypeScript and Styled Components"
+        ></meta>
+      </Head>
       <Component {...pageProps} />
       <GlobalStyle />
     </>
